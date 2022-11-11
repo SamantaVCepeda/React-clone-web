e72a1c68c160d48ff5794c1b9348119367969897
SamantaVCepeda <samantavcepeda@gmail.com>
2022-11-10 23:14:28 -0300

    termine

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -5,7 +5,7 @@ function Testimonio(props){
   return (
     <div className='contenedor-testimonio'>
       <img className= 'imagen-testimonio' src= { require(`../imagenes/${props.imagen}.png`)}
-        alt= 'foto Shawn'/>
+        alt= 'foto de estudiante' />
       <div className='contenedor-texto-testimonio'>
         <p className='nombre-testimonio'><strong>{props.nombre}</strong> en {props.pais}</p>
         <p className='cargo-testimonio'>{props.cargo} en <strong>{props.empresa}</strong></p>
